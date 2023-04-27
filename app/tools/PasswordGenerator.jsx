9ae306637cd6b85be2780c641997b3c9440e251c
MuraJJDG <juanj.duque@upb.edu.co>
2023-04-27 16:45:39 -0500

    Correciones de texto a la funcion discound y password generator funcionando

--- app/tools/PasswordGenerator.jsx
+++ app/tools/PasswordGenerator.jsx
@@ -1,10 +1,70 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  Alert,
+  Clipboard,
+} from "react-native";
+import Navbar from "../../components/common/Navbar/Navbar";
+import Input from "../../components/common/Input/Input";
+import CopyText from "../../components/common/CopyText/CopyText";
+import { COLORS, SIZES } from "../../constants/theme";
+import styles from "../../styles/main.styles";
 
 function PasswordGenerator() {
+  const [password, setPassword] = useState('');
+  const [passwordLength, setPasswordLength] = useState(12);
+
+  const handleChange = (value) => {
+    setPasswordLength(parseInt(value));
+  }
+
+  const generatePassword = () => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    let generatedPassword = '';
+    for (let i = 0; i < passwordLength; i++) {
+      generatedPassword += chars[Math.floor(Math.random() * chars.length)];
+    }
+    setPassword(generatedPassword);
+  }
+
+  const copyPassword = () => {
+    Clipboard.setString(password);
+    Alert.alert('La contraseña ha sido copiada al portapapeles');
+  }
+
+  const handleCopyToClipboard = () => {
+    Clipboard.setString(password);
+  };
+
   return (
-    <View>
-      <Text>PasswordGenerator</Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      <Navbar />
+      <View style={{ padding: SIZES.medium, gap: SIZES.medium }}>
+        <Text style={styles.h1}>Generador contrasenas</Text>
+        <Text>Ingresa el numero de caracteres de la contrasena</Text>
+        <Input
+          handleChange={handleChange}
+          value={passwordLength}
+          keyboardType="numeric"
+        />
+
+        <Button
+          color={COLORS.red}
+          title='Generar contrasena'
+          onPress={generatePassword}
+        />
+       {password ? (
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: SIZES.small }}>
+            <Text style={{ flex: 1 }}>{password}</Text>
+            <CopyText onPress={copyPassword} handleCopy={handleCopyToClipboard} />
+          </View>
+        ) : null}
+      </View>
+    </SafeAreaView>
   );
+
 }
 export default PasswordGenerator;
